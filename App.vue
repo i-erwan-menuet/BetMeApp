431e17ab51b57c417fac21c48083852fc6d7fcb6
Erwan Menuet <erwanmenuetts1@gmail.com>
2019-12-01 21:21:38 +0100

    Side menu basic behavior

---
+++ App.vue
@@ -0,0 +1,39 @@
+<template>
+  <view class="container">
+    <StatusBar v-bind:color="statusBarColor"/>
+    <NavBar v-bind:title="currentTitle"/>
+  </view>
+</template>
+
+<script>
+    //import { Ionicons } from "react-native-elements"
+    import { Ionicons } from "@expo/vector-icons";
+    import Vue from "vue-native-core";
+
+    Vue.component("ionicons", Ionicons)
+
+    import StatusBar from "./Components/StatusBar.vue"
+    import NavBar from "./Components/NavBar.vue"
+
+    export default{
+      components:{
+        StatusBar,
+        NavBar,
+        Ionicons
+      },
+      data (){
+        return{
+            statusBarColor: "green",
+            currentTitle: "Home"
+        };
+      }
+    }
+</script>
+
+
+<style>
+.container {
+  /*background-color: white;*/
+  flex: 1;
+}
+</style>
---
+++ Components/StatusBar.vue
@@ -0,0 +1,27 @@
+<template>
+  <view class="statusBar" v-bind:style="getStyle()"></view>
+</template>
+
+<script>
+  export default {
+    props:['color'],
+    data: function() {
+      return {
+      };
+    },
+    methods:{
+      getStyle: function(){
+        return {
+          backgroundColor: this.color
+        }
+      }
+    }
+  };
+</script>
+
+<style>
+  .statusBar{
+    /*background-color: white;/*By default*/
+    height:20px;
+  }
+</style>
